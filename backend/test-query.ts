7589ed4c8895489516ad63bc777cb7ded3d34a2f
masOOm12342 <triple.s02gaming@gmail.com>
2026-08-30 08:46:16 +0530

    chore: remove hardcoded member seed data and migration scripts

--- backend/test-query.ts
+++
@@ -1,9 +0,0 @@
-import { query } from './src/db/index';
-
-async function main() {
-  const visitors = await query('SELECT * FROM visitors');
-  console.log(JSON.stringify(visitors, null, 2));
-  process.exit(0);
-}
-
-main();
